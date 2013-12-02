f56386d96bac93a1018566b7fe06be60be76912f
Radosław Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2013-12-02 13:45:55 +0100

    Refine XML stream formatter

--- src/ejabberd_trace_format.erl
+++ src/ejabberd_trace_format.erl
@@ -2,22 +2,20 @@
 
 -export([stream/2]).
 
-stream(end_of_trace, _Opts) ->
-    true;
 stream({trace, _Pid, 'receive',
         {'$gen_event', {xmlstreamstart, _, _} = StreamStart}}, _Opts) ->
-    io:format("~p~n", [exml:to_iolist(StreamStart)]);
+    io:format("in :~n~ts~n~n", [exml:to_iolist(StreamStart)]);
 stream({trace, _Pid, 'receive',
         {'$gen_event', {xmlstreamelement, Elem}}}, _Opts) ->
-    io:format("~p~n", [exml:to_iolist(xmlelement_to_xmlel(Elem))]);
+    io:format("in :~n~ts~n~n", [exml:to_iolist(xmlelement_to_xmlel(Elem))]);
 stream({trace, _Pid, 'receive',
         {'$gen_event', {xmlstreamend, _} = StreamEnd}}, _Opts) ->
-    io:format("~p~n", [exml:to_iolist(StreamEnd)]);
+    io:format("in :~n~ts~n~n", [exml:to_iolist(StreamEnd)]);
 stream({trace, _Pid, call,
         {ejabberd_c2s, send_text, [_State, Msg]}}, _Opts) ->
-    io:format("~p~n", [Msg]);
+    io:format("out :~n~ts~n~n", [Msg]);
 stream(_, _Opts) ->
-    false.
+    ok.
 
 xmlelement_to_xmlel({xmlelement, Name, Attrs, Children}) ->
     {xmlel, Name, Attrs, [xmlelement_to_xmlel(C) || C <- Children]};
