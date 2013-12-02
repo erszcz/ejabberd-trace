1233149537922bef2d68dacfbd704f7d81b35fb8
Radosław Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2013-12-02 15:52:56 +0100

    Pretty print XML using stream formatter

--- src/ejabberd_trace_format.erl
+++ src/ejabberd_trace_format.erl
@@ -4,13 +4,13 @@
 
 stream({trace, _Pid, 'receive',
         {'$gen_event', {xmlstreamstart, _, _} = StreamStart}}, _Opts) ->
-    io:format("in :~n~ts~n~n", [exml:to_iolist(StreamStart)]);
+    io:format("in :~n~ts~n~n", [to_iolist(StreamStart)]);
 stream({trace, _Pid, 'receive',
         {'$gen_event', {xmlstreamelement, Elem}}}, _Opts) ->
-    io:format("in :~n~ts~n~n", [exml:to_iolist(xmlelement_to_xmlel(Elem))]);
+    io:format("in :~n~ts~n~n", [to_iolist(xmlelement_to_xmlel(Elem))]);
 stream({trace, _Pid, 'receive',
         {'$gen_event', {xmlstreamend, _} = StreamEnd}}, _Opts) ->
-    io:format("in :~n~ts~n~n", [exml:to_iolist(StreamEnd)]);
+    io:format("in :~n~ts~n~n", [to_iolist(StreamEnd)]);
 stream({trace, _Pid, call,
         {ejabberd_c2s, send_text, [_State, Msg]}}, _Opts) ->
     io:format("out :~n~ts~n~n", [Msg]);
@@ -21,3 +21,6 @@ xmlelement_to_xmlel({xmlelement, Name, Attrs, Children}) ->
     {xmlel, Name, Attrs, [xmlelement_to_xmlel(C) || C <- Children]};
 xmlelement_to_xmlel(Other) ->
     Other.
+
+to_iolist(Elem) ->
+    exml:to_pretty_iolist(Elem).
