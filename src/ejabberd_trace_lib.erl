d42041d393c4b3591537ccb2e02dce1e4bb32a4c
Radosław Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2014-01-03 16:44:18 +0100

    Fix and test Jid/Bind getters

--- src/ejabberd_trace_lib.erl
+++ src/ejabberd_trace_lib.erl
@@ -45,8 +45,7 @@ get_jid(_) ->
 trace_handler(Trace, #tstate{} = TState)
   when ?IS_C2S_TRIGGER(Trace) ->
     Pid = element(2, Trace),
-    %% TODO: extract BindResult from Trace
-    BindResult = 1/0,
+    BindResult = get_bind_result(Trace),
     ?DEBUG(">>>>> c2s trigger matched pid=~p~n", [Pid]),
     handle_trace(Trace, Pid, TState),
     case get_jid(BindResult) of
@@ -59,6 +58,13 @@ trace_handler(Trace, #tstate{} = TState)
 trace_handler(Trace, TState) ->
     handle_trace(Trace, element(2, Trace), TState),
     TState.
+
+get_bind_result({_, _, call, {ejabberd_c2s, send_element, _}} = Trace) ->
+    {trace, _Pid, call, {ejabberd_c2s, send_element, [_, BindResult]}} = Trace,
+    BindResult;
+get_bind_result({_, _, send, {bosh_reply, _}, _} = Trace) ->
+    {trace, _, send, {bosh_reply, {_, _, _, [BindResult]}}, _} = Trace,
+    BindResult.
 
 handle_trace(Trace, Pid, #tstate{server = TraceServer} = TState) ->
     Action = ejabberd_trace_server:get_action(TraceServer, Pid),
@@ -142,25 +148,95 @@ maybe_disable_cache(TraceServer) ->
 
 -ifdef(TEST).
 -include_lib("eunit/include/eunit.hrl").
+-define(_a(Condition), ?_assert(Condition)).
+-define(_eq(E, I), ?_assertEqual(E, I)).
+-define(a(Condition), ?assert(Condition)).
 
-example_bind(Jid) ->
-    {xmlelement, "iq",
-     [{"id", "bind_1"},{"type", "result"}],
-     [{xmlelement, "bind",
-       [{"xmlns", "urn:ietf:params:xml:ns:xmpp-bind"}],
-       [{xmlelement, "jid",[],
-         [{xmlcdata, Jid}]}]}]}.
-
-%% TODO
 c2s_trigger_test() ->
-    error(unimplemented).
+    T = c2s_trace(),
+    ?a(if
+           ?IS_C2S_TRIGGER(T) -> true;
+           true -> false
+       end).
 
-%% TODO
 bosh_trigger_test() ->
-    error(unimplemented).
+    T = bosh_trace(),
+    ?a(if
+           ?IS_BOSH_TRIGGER(T) -> true;
+           true -> false
+       end).
 
-get_jid_test() ->
-    Jid = "qwe@localhost/x3",
-    ?assertEqual(Jid, get_jid(example_bind(Jid))).
+get_bind_result_test_() ->
+    G = fun get_bind_result/1,
+    [?_a(is_bind_result(G(c2s_trace()))),
+     ?_a(is_bind_result(G(bosh_trace())))].
+
+get_jid_test_() ->
+    G = fun get_bind_result/1,
+    J = fun get_jid/1,
+    [?_eq(<<"asd@localhost/x3">>, J(G(c2s_trace()))),
+     ?_eq(<<"carol@localhost/escalus-default-resource">>, J(G(bosh_trace())))].
+
+is_bind_result({_, _, _, [{_, _, _, [Jid]}]} = BindIQResult)
+  when ?IS_IQ(BindIQResult),
+       ?IS_BIND(hd(?EL(4, BindIQResult))),
+       ?IS_JID(Jid) ->
+    true;
+is_bind_result(_) ->
+    false.
+
+%% Fixtures
+
+c2s_trace() ->
+    {trace,pid_port_fun_ref,call,
+     {ejabberd_c2s,send_element,
+      [{state,
+        {socket_state,ejabberd_tls,
+         {tlssock,pid_port_fun_ref,pid_port_fun_ref},
+         pid_port_fun_ref},
+        ejabberd_socket,pid_port_fun_ref,false,"3764444141",
+        {sasl_state,<<"jabber">>,<<"localhost">>,<<>>,
+         pid_port_fun_ref,
+         pid_port_fun_ref,
+         pid_port_fun_ref,cyrsasl_digest,
+         {state,5,"3011314780",<<"asd">>,<<>>,
+          pid_port_fun_ref,
+          pid_port_fun_ref,
+          ejabberd_auth_internal,<<"localhost">>}},
+        c2s,c2s_shaper,false,true,false,true,
+        [verify_none,{certfile,"/tmp/server.pem"}],
+        true,undefined,<<"asd">>,<<"localhost">>,<<>>,undefined,
+        {0,nil},
+        {0,nil},
+        {0,nil},
+        {0,nil},
+        [],undefined,undefined,undefined,false,
+        {userlist,none,[],false},
+        unknown,ejabberd_auth_internal,
+        {{127,0,0,1},61957},
+        [],"en"},
+       {xmlel,<<"iq">>,
+        [{<<"id">>,<<"bind_1">>},{<<"type">>,<<"result">>}],
+        [{xmlel,<<"bind">>,
+          [{<<"xmlns">>,<<"urn:ietf:params:xml:ns:xmpp-bind">>}],
+          [{xmlel,<<"jid">>,[],
+            [{xmlcdata,<<"asd@localhost/x3">>}]}]}]}]}}.
+
+bosh_trace() ->
+    {trace,pid_port_fun_ref,send,
+     {bosh_reply,
+      {xmlel,<<"body">>,
+       [{<<"sid">>,<<"dcac3f89e562c670d31ad7cc22f8504103a63b19">>},
+        {<<"xmlns">>,<<"http://jabber.org/protocol/httpbind">>}],
+       [{xmlel,<<"iq">>,
+         [{<<"id">>,<<"370c1c72e14f20dd1262862343f1031a">>},
+          {<<"type">>,<<"result">>}],
+         [{xmlel,<<"bind">>,
+           [{<<"xmlns">>,
+             <<"urn:ietf:params:xml:ns:xmpp-bind">>}],
+           [{xmlel,<<"jid">>,[],
+             [{xmlcdata,
+               <<"carol@localhost/escalus-default-resource">>}]}]}]}]}},
+     pid_port_fun_ref}.
 
 -endif.
